175b88d44e56a554b5578f785cc75845e9d28b0c
luhbaldon <luisa.grassmann@escola.pr.gov.br>
2024-12-06 12:16:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ atual.classList.remove('ativo');
 document.getElementById(proximoPasso).classList.add('ativo');
    })
 })
+
+// Reinicia o jogo ao clicar no botão de reinício
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+    const atual = document.querySelector('.ativo');
+    atual.classList.remove('ativo');
+    document.getElementById('passo-0').classList.add('ativo');
+    });
+    }
